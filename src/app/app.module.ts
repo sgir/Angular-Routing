eaa7e470f4206573d7ae00f5bc89309f110753c2
Sathya Prabha Girish <sathya.prabha.girish@sap.com>
2018-11-08 17:26:29 -0500

    Milestone 3: Heroes feature  and its Routing Module
    Organize the app and routes into feature areas using modules.
    Navigate imperatively from one component to another.
    Pass required and optional information in route parameters.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { HeroListComponent } from './hero-list/hero-list.component';
+import { HeroListComponent } from './heroes/hero-list/hero-list.component';
 import {FormsModule} from '@angular/forms';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { AppRoutingModule } from './/app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
