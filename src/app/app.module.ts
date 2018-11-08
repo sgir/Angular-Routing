3877784125d71fa164c705947b037edc90333f30
Sathya Prabha Girish <sathya.prabha.girish@sap.com>
2018-11-08 10:31:33 -0500

    Milestone 2: Routing module
    moving the routing information into a special-purpose module called a Routing Module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
 import {FormsModule} from '@angular/forms';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './/app-routing.module';
 
-
-const appRoutes: Routes = [
-
-  {path: 'crisis-center', component: CrisisListComponent},
-  {path: 'heroes', component: HeroListComponent},
-  {path: '', redirectTo: '/heroes', pathMatch: 'full'},
-  {path: '**', component: PageNotFoundComponent}
-];
 
 @NgModule({
   declarations: [
@@ -27,7 +19,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes, {enableTracing: true})
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
