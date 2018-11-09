e09630f8381fc4ab82c09f467a325f35f2a05fa9
Sathya Prabha Girish <sathya.prabha.girish@sap.com>
2018-11-09 10:29:51 -0500

    move the Hero Feature modules to heroes-routing.module.ts

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HeroListComponent} from './hero-list/hero-list.component';
-import {HeroDetailComponent} from './hero-detail/hero-detail.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-const routes: Routes = [
+const heroesRoutes: Routes = [
   {path: 'heroes', component: HeroListComponent},
   {path: 'hero/:id', component: HeroDetailComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(heroesRoutes)],
   exports: [RouterModule]
 })
 export class HeroesRoutingModule { }
